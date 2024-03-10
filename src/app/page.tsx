01e42e5672c7cb9938ae3859d5b2afd9c806a248
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-03-10 21:57:13 +0600

    Home Page Section 01 Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+import Thunder from "@/components/icons/thunder";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main className="py-10 bg-[url('/img/home-page-bg.png')]">
+        <section className="container mx-auto md:py-32 py-10">
+          <div className="bg-[#FCFCFD] rounded md:p-5 p-2 max-w-fit w-full mx-auto flex items-center gap-5">
+            <div className="bg-[#FAE3C3] rounded md:p-3 p-1">
+              <Thunder />
+            </div>
+            <h1 className="text-[#1A1A1A] md:text-4xl text-xl font-bold">
+              <span className="text-themePrimary">Unlock</span> Your Creative
+              Potential
+            </h1>
+          </div>
+          <p className="text-center mt-5 max-w-[550px] w-full mx-auto">
+            Learn from Industry Experts and Enhance Your Skills & Share what you
+            know{";"} we{"'"}ll help you get paid for it
+          </p>
+          <div className="flex md:flex-row flex-col items-center gap-6 justify-center mt-8">
+            <Button asChild size={"lg"}>
+              <Link href={""}>Explore Sevices</Link>
+            </Button>
+            <Button asChild size={"lg"} className="bg-themePrimary">
+              <Link href={""}>View Pricing</Link>
+            </Button>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -52,6 +52,18 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        themePrimary: {
+          DEFAULT: "var(--theme-primary)",
+        },
+        themeStudentPrimary: {
+          DEFAULT: "var(--theme-student-primary)",
+        },
+        themeTeacherPrimary: {
+          DEFAULT: "var(--theme-teacher-primary)",
+        },
+        themeAdminPrimary: {
+          DEFAULT: "var(--theme-admin-primary)",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -75,6 +87,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
