1c108831c66cb678fdc50f4ab3aaeb4f26d03f98
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-03-17 16:23:34 +0600

    Add Pagination On Admin Users/allUsers.tsx

--- src/app/(main)/admin/users/_components/allUsers.tsx
+++ src/app/(main)/admin/users/_components/allUsers.tsx
@@ -1,9 +1,48 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import React from "react";
 
 type Props = {};
 
 const AllUsers = (props: Props) => {
-  return <div>AllUsers</div>;
+  const prevPage = 1;
+  const nextPage = 2;
+
+  return (
+    <section>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href={`?page=${prevPage}`} />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="?page=1">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="?page=2" isActive>
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="?page=3">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href={`?page=${nextPage}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </section>
+  );
 };
 
 export default AllUsers;
