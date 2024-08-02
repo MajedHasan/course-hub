a9aab1e6db13406abddfce66a630e77d9afa53de
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-08-02 11:53:06 +0600

    Marketplace/Courses Page First Section Done

--- src/app/site/marketplace/layout.tsx
+++ src/app/site/marketplace/layout.tsx
@@ -1,5 +1,9 @@
 const MarketplaceLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="pb-10 bg-[url('/img/home-page-bg.png')] bg-[no-repeat] bg-[center center] bg-[cover]">
+      {children}
+    </main>
+  );
 };
 
 export default MarketplaceLayout;
--- src/app/site/marketplace/page.tsx
+++ src/app/site/marketplace/page.tsx
@@ -3,7 +3,23 @@ import React from "react";
 type Props = {};
 
 const MarketplacePage = (props: Props) => {
-  return <div>Marketplace</div>;
+  return (
+    <>
+      <section className="bg-[#555555] pt-32 pb-32 px-10"></section>
+      <section className="-mt-40">
+        <div className="bg-[#FFFFFF] py-20 px-10 container mx-auto rounded-lg text-center">
+          <h1 className="text-6xl font-bold relative w-fit mx-auto">
+            <span className="block absolute w-16 h-16 rounded-full bg-[#DAA520] z-10 -left-6 -top-6"></span>{" "}
+            <span className="relative z-20">Marketplace</span>
+          </h1>
+        </div>
+      </section>
+
+      <main>
+        <aside></aside>
+      </main>
+    </>
+  );
 };
 
 export default MarketplacePage;
